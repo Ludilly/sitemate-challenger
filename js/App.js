a6c32d2c995201306cf7cff32edfddd0591861a8
Ludilly <luiza.dilly@gmail.com>
2022-01-22 18:57:42 -0300

    remove useless code

--- js/App.js
+++
@@ -1,9 +0,0 @@
-const App = () => {
-    const App = document.createElement("div")
-   
-    App.setAttribute("class", "App")
-    App.append(fetchIntel())
-    
-    return App
-}
-export default App
--- js/Dialog/index.js
+++ js/Dialog/index.js
@@ -21,8 +21,6 @@ function detachModalListeners(modalElm) {
 function toggleModal() {
 
   const currentState = modal.style.display;
-
-    // If modal is visible, hide it. Else, display it.
   
   if (currentState === 'none') {
     modal.style.display = 'block';
